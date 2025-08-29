76ddcce0c2a99e292a2f1515c1adf622f42b62d0
unknown <georgeenesi81@gmail.com>
2025-08-29 12:59:05 -0700

    nw

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
+
+# Install all dependencies (including dev)
+echo "Installing all dependencies..."
+composer install --working-dir=/var/www/html
 
 echo "Caching config..."
 php artisan config:cache
@@ -9,8 +12,21 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
+echo "generating app key..."
+php artisan key:generate --force
+
 echo "Running migrations..."
 php artisan migrate --force
+
+echo "database seeder seeding super-admin user"
+php artisan db:seed --force
+
+echo "Removing dev dependencies..."
+composer install --no-dev --working-dir=/var/www/html
+
+echo 'Running app schedule'
+php artisan schedule:run --no-interaction --quiet
+sleep 60
 
 echo "Running the queue ..."
 php /var/www/artisan queue:work --verbose --tries=3 --timeout=180
